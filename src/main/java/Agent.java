854a22ad35401e465ac9b113becc15f01ced68a6
KastorRico <sandra.pylypiva@gmail.com>
2020-11-11 14:20:23 +0200

    Some changes for agent anim

--- src/main/java/Agent.java
+++ src/main/java/Agent.java
@@ -6,11 +6,24 @@ import org.logicng.solvers.MiniSat;
 import org.logicng.solvers.SATSolver;
 import org.logicng.solvers.SolverState;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Agent {
     final FormulaFactory f = new FormulaFactory();
     final PropositionalParser p = new PropositionalParser(f);
     final SATSolver miniSat = MiniSat.miniSat(f);
 
+    static final int AGENT_ANIM_IMAGE = 4;
+    private static final int ANIMATION_STEPS = 5;
+    private static Image agentR,agentL;
+    Board board;
+    private int directionAgentX, directionAgentY;
+    public int agentX, agentY;
+    public int agentXOld, agentYOld;
+    int additionAnimationY = 0, additionAnimationX = 0;
+    int animationCount = 0;
+    
     public Agent() throws ParserException {
         initKnowledgeBase();
     }
@@ -47,5 +60,48 @@ public class Agent {
         newSat.loadState(state);
         newSat.add(p.parse(info));
         return newSat.sat() == Tristate.TRUE;
+    }
+
+    void initAgentImages() {
+        agentR = new ImageIcon("images/spyR.png").getImage();
+        agentL = new ImageIcon("images/spyL.png").getImage();
+    }
+
+
+    private void moveAgentTo(int j, int i) {
+        directionAgentX = (i - agentX);
+        directionAgentY = (j - agentY);
+        agentXOld = agentX;
+        agentYOld = agentY;
+        agentX = i;
+        agentY = j;
+    }
+    void animationMoveAgent() {
+        Point point = null;
+        if (point == null)
+            board.stop();
+        else
+            moveAgentTo(point.y, point.x);
+    }
+
+    public void drawAgent(Graphics2D g2d) {
+        additionAnimationX = -1 * directionAgentX * (ANIMATION_STEPS - animationCount) * (Board.BLOCK_SIZE / ANIMATION_STEPS);
+        additionAnimationY = -1 * directionAgentY * (ANIMATION_STEPS - animationCount) * (Board.BLOCK_SIZE / ANIMATION_STEPS);
+
+        int x = agentX * Board.BLOCK_SIZE + additionAnimationX;
+        int y = agentY * Board.BLOCK_SIZE + additionAnimationY;
+        if (directionAgentX == -1) {
+            g2d.drawImage(agentL, x, y,Board.BLOCK_SIZE,Board.BLOCK_SIZE, board);
+        } else if (directionAgentX == 1) {
+            g2d.drawImage(agentR, x, y,Board.BLOCK_SIZE,Board.BLOCK_SIZE, board);
+        }
+    }
+
+    public void step(Graphics2D g2d) {
+        drawAgent(g2d);
+        animationCount %= ANIMATION_STEPS;
+        if (animationCount++ == 0) {
+            animationMoveAgent();
+        }
     }
 }
